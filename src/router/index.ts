a26d75ed3c8c67ef56641a86e7ef295728ba5dfd
cyeext <cyeext@sjtu.edu.cn>
2025-09-21 09:55:13 +0800

    3 创建项目路由

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
+const routes:RouteRecordRaw[] = [
+    {
+        path:'/',
+        name:'home',
+        component:()=>import('../views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/About.vue')
+    }
+]
+
+const router = createRouter({
+    routes,
+    history:createWebHistory()
+})
+
+export default router
