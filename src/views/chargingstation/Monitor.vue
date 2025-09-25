36af8533514fe5cb2edb2bc7f250efa2a01a6c75
cyeext <cyeext@sjtu.edu.cn>
2025-09-25 09:48:10 +0800

    37 充电站监控页面基本结构开发

--- src/views/chargingstation/Monitor.vue
+++ src/views/chargingstation/Monitor.vue
@@ -1,11 +1,62 @@
 <template>
-    充电站监控
+    <el-card>
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <el-input v-model="formParams.input" placeholder="请输入站点名称、ID" style="max-width: 600px;"
+                    class="input-with-select">
+                    <template #append>
+                        <el-select v-model="select" placeholder="" style="width: 115px">
+                            <el-option label="按名称查询" value="name" />
+                            <el-option label="按ID查询" value="id" />
+                        </el-select>
+                    </template>
+                </el-input>
+            </el-col>
+            <el-col :span="6">
+                <el-select placeholder="充电站状态" v-model="formParams.value">
+                    <el-option label="全部" :value="1"></el-option>
+                    <el-option label="使用中" :value="2"></el-option>
+                    <el-option label="空闲中" :value="3"></el-option>
+                    <el-option label="维护中" :value="4"></el-option>
+                    <el-option label="待维修" :value="5"></el-option>
+                </el-select>
+            </el-col>
+            <el-col :span="6">
+                <el-button type="primary">查询</el-button>
+                <el-button>重置</el-button>
+            </el-col>
+        </el-row>
+    </el-card>
+    <el-card class="mt">
+        <el-row>
+            <el-col :span="6">
+                <el-statistic title="累计充电量(度)" :value="268900" />
+            </el-col>
+            <el-col :span="6">
+                <el-statistic title="累计充电次数(次)" :value="1389" />
+            </el-col>
+            <el-col :span="6">
+                <el-statistic title="服务区域(个)" :value="88" />
+            </el-col>
+            <el-col :span="6">
+                <el-statistic title="累计效益(元)" :value="5622178" />
+            </el-col>
+        </el-row>
+    </el-card>
+    <el-card class="mt">
+        <el-button type="primary" icon="Plus">新增充电站</el-button>
+    </el-card>
 </template>
 
 <script setup lang="ts">
+    import { reactive, ref } from 'vue';
+
+    const select = ref("name")
+    const formParams = reactive({
+        input: "",
+        value: 1
+    })
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
