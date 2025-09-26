356c423e085adb026e2d310955468ba611d9053e
cyeext <cyeext@sjtu.edu.cn>
2025-09-26 16:03:16 +0800

    51 充电桩管理页面静态结构开发

--- src/views/chargingstation/Management.vue
+++ src/views/chargingstation/Management.vue
@@ -1,9 +1,99 @@
 <template>
-    充电桩管理
+    <el-card>
+        <el-select filterable placeholder="Select" style="width: 240px">
+        </el-select>
+    </el-card>
+    <el-card class="mt">
+        <el-radio-group size="large">
+            <el-radio-button label="全部" value="0" />
+            <el-radio-button label="空闲中" value="1" />
+            <el-radio-button label="充电中" value="2" />
+            <el-radio-button label="连接中" value="3" />
+            <el-radio-button label="排队中" value="4" />
+            <el-radio-button label="已预约" value="5" />
+            <el-radio-button label="故障/离线" value="6" />
+        </el-radio-group>
+    </el-card>
+    <el-card class="mt">
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <div class="item">
+                    <div class="pic">
+                        <p>空闲中</p>
+                        <img :src="free" width="100px">
+                        <p>0%</p>
+                    </div>
+                    <div class="info">
+                        <h3>CD1002</h3>
+                        <hr class="mb">
+                        <p>电压: 314v</p>
+                        <p>电流: 212.2A</p>
+                        <p>功率: 21kW</p>
+                        <p>温度: 32℃</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <div class="divider"></div>
+                    <div>
+                        <p class="fl ml" style="font-size: 12px; color: #999;">暂无预计</p>
+                        <div class="fr">
+                            <el-button size="small">维保记录</el-button>
+                            <el-button size="small" type="primary" class="mr">使用记录</el-button>
+                        </div>
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+    </el-card>
+    <el-card class="mt">
+
+    </el-card>
 </template>
 
 <script setup lang="ts">
-
+    import free from '@/assets/free.png'
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+    .item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        background-color: rgb(247, 251, 254);
+        padding: 20px;
+        border-radius: 10px 10px 0 0;
+        margin-top: 20px;
+
+        .pic {
+            p {
+                width: 76px;
+                text-align: center;
+                margin-bottom: 10px;
+                color: rgb(61, 187, 146)
+            }
+        }
+
+        .info {
+            color: #999;
+            margin-left: 30px;
+            line-height: 26px;
+            margin-top: -10px;
+        }
+
+    }
+
+    .btn {
+        width: 100%;
+        height: 50px;
+        line-height: 50px;
+        background-color: #f7fbfe;
+
+        .divider {
+            background-color: #f4f4f4;
+            height: 2px;
+            width: 95%;
+            margin: auto;
+        }
+    }
+</style>
